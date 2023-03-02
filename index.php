ffa3d497d907b079fbc1219b0289dccb27550bed
PDO_Cinema <florian.leininger01@gmail.com>
2023-03-02 13:50:26 +0100

    + style et correction affichage durée

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 
 // CONNEXION A LA BASE DE DONNEES
@@ -16,7 +30,7 @@ catch (Exception $e) {
 
 // TRAITEMENT
 
-$sqlQuery = "SELECT titre_film, annee_film, nom_realisateur, prenom_realisateur, CONVERT(duree_film, TIME) AS duree_heure
+$sqlQuery = "SELECT titre_film, annee_film, nom_realisateur, prenom_realisateur, TIME_FORMAT(SEC_TO_TIME(duree_film*60), '%H:%i') AS duree_heure
             FROM film
             INNER JOIN realisateur r ON r.id_realisateur = film.id_realisateur
             ORDER BY annee_film DESC";
